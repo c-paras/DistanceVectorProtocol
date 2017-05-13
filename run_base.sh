600ca4a23f73a90d4dc8ea2108075dc8ef015472
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-05-13 17:41:58 +1000

    added another topology for testing and adapted testing script

--- run_base.sh
+++ run_base.sh
@@ -1,14 +1,26 @@
 #!/bin/sh
 #Written by Costa Paraskevopoulos in May 2017
-#Runs the distance vector protocol at each node in the sample topology
+#Runs the distance vector routing protocol at each node in the given topology
+
+if test $# -ne 1
+then
+	echo "Usage: $0 <topology>" >&2
+	exit 1
+else
+	topology=$1
+fi
 
 prog=Dvr.py
 port_num=2000
 
-for router in A B C D E F
+for router in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 do
+	if ! test -e $topology/config$router.txt
+	then
+		break
+	fi
 	echo "Running router $router..."
-	./$prog $router $port_num topology1/config$router.txt &
+	./$prog $router $port_num $topology/config$router.txt &
 	port_num=$(($port_num + 1))
 	sleep 1
 done
