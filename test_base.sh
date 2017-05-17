71e353aa9cf874b0250bfee32bf6eeb29340eccb
Costa Paraskevopoulos <costa.paras1@gmail.com>
2017-05-17 17:03:15 +1000

    updated testing script

--- test_base.sh
+++ test_base.sh
@@ -10,23 +10,35 @@ else
 	topology=$1
 fi
 
+if ! test -d "$topology" || ! test -x "$topology"
+then
+	echo "$0: cannot access '$topology'" >&2
+	exit 1
+fi
+
 prog=Dvr.py
 port_num=2000
 
 for router in A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 do
-	if ! test -e $topology/config$router.txt
+	if ! test -e "$topology/config$router.txt"
 	then
 		break
 	fi
 	echo "Running router $router..."
-	./$prog $router $port_num $topology/config$router.txt &
+	./$prog $router $port_num "$topology/config$router.txt" &
 	port_num=$(($port_num + 1))
 	sleep 1
 done
 
+if test $port_num -eq 2000 -a `ps 2000 | wc -l` -eq 1
+then
+	echo "$0: no configuration files in '$topology'" >&2
+	exit 1
+fi
+
 echo
-echo "Press any key to terminate all routers"
+echo "Press ENTER to terminate all routers"
 echo
 while true
 do
